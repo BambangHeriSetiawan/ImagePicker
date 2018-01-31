3d58f28f5e592ddeaae09318ffb5de513dd96861
Bambang Heri Setiawan <setiawan.heri.bambang@gmail.com>
2018-01-31 09:13:53 +0700

    add Rx Permissions

---
+++ app/src/main/java/com/simx/imagepicker/Apps.java
@@ -0,0 +1,23 @@
+package com.simx.imagepicker;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by simxd on 1/31/2018.
+ */
+
+public class Apps extends Application {
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+
+    public Context getContext(){
+        return context;
+    }
+}
--- app/src/main/java/com/simx/imagepicker/MainActivity.java
+++ app/src/main/java/com/simx/imagepicker/MainActivity.java
@@ -1,13 +1,32 @@
 package com.simx.imagepicker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.img)
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnCamera)
+    public void onBtnCameraClicked() {
+
+    }
+
+    @OnClick(R.id.btnGallery)
+    public void onBtnGalleryClicked() {
+
     }
 }
